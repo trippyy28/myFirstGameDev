fa167b5c252e058ff870d01a135ada0c21b950a4
user <user@users-MacBook-Pro.local>
2022-08-15 14:51:39 +0300

    first commit

---
+++ ShadowDog/script.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+console.log(ctx);
+
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+//vertical
+let frameX = 2;
+// horizontal
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.fillRect(x, 50, 100, 100);
+
+  ctx.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+  if (gameFrame % staggerFrames == 0) {
+    if (frameX < 6) frameX++;
+    else frameX = 0;
+  }
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
