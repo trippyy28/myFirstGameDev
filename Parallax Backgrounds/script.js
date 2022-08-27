017ac03b7d15d9119f7c7116de09d3df982418f4
user <user@users-MacBook-Pro.local>
2022-08-27 09:55:20 +0300

    adding new project Parrllax Background

--- Parallax Backgrounds/script.js
+++ Parallax Backgrounds/script.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-let gameSpeed = 15;
+let gameSpeed = 10;
+// let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "layer-1.png";
@@ -15,41 +16,61 @@ backgroundLayer4.src = "layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "layer-5.png";
 
+const slider = document.getElementById("slider");
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById("showGameSpeed");
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener("change", function (e) {
+  console.log(e.target.value);
+  gameSpeed = e.target.value;
+  showGameSpeed.innerHTML = e.target.value;
+});
+
 class Layer {
   constructor(image, speedMoifier) {
     this.x = 0;
     this.y = 0;
     this.width = 2400;
     this.height = 700;
-    this.x2 = this.width;
     this.image = image;
     this.speedMoifier = speedMoifier;
     this.speed = gameSpeed * this.speedMoifier;
   }
   update() {
     this.speed = gameSpeed * this.speedMoifier;
-    if (this.x < -this.width) {
-      this.x = this.width + this.x - this.speed;
+    if (this.x <= -this.width) {
+      this.x = 0;
     }
-    if (this.x < -this.width) {
-      this.x2 = this.width + this.x2 - this.speed;
-    }
-    this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
+    this.x = this.x - this.speed;
+    // this.x = (gameFrame * this.speed) % this.width;
   }
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y2, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  layer4.update();
-  layer4.draw();
-
+  gameObjects.forEach((object) => {
+    object.update();
+    object.draw();
+  });
+  // gameFrame--;
   requestAnimationFrame(animate);
 }
 animate();
