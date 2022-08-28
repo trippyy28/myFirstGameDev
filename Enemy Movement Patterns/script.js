723fdf929daa6b213c24b387c1e4bf935d6a26d9
user <user@users-MacBook-Pro.local>
2022-08-28 17:33:14 +0300

    adding new project Enemy Movement

---
+++ Enemy Movement Patterns/script.js
@@ -0,0 +1,72 @@
+//init
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+let numberOfEnemis = 100;
+const enemisArray = [];
+
+let gameFrame = 0;
+// enemy1 = {
+//   x: 10,
+//   y: 50,
+//   width: 100,
+//   height: 50,
+// };
+
+class Enemy {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "./enemies/enemy1.png";
+    this.speed = Math.random() * 4 - 10;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 4 + 1);
+  }
+  update() {
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
+    //animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+//const enemy1 = new Enemy()
+
+for (let i = 0; i < numberOfEnemis; i++) {
+  enemisArray.push(new Enemy());
+}
+
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemisArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
