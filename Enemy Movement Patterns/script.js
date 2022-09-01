24cce7c3208804fb9a55c24a02b45828f21e5c34
user <user@users-MacBook-Pro.local>
2022-09-01 03:15:28 +0300

    adding sin and cos

--- Enemy Movement Patterns/script.js
+++ Enemy Movement Patterns/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-let numberOfEnemis = 10;
+let numberOfEnemis = 30;
 const enemisArray = [];
 
 let gameFrame = 0;
@@ -17,20 +17,32 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "./enemies/enemy1.png";
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
-    this.width = this.spriteWidth / 2.5;
-    this.height = this.spriteHeight / 2.5;
-    //this.speed = Math.random() * 4 - 10;
-    this.x = Math.random() * (canvas.width - this.width);
-    this.y = Math.random() * (canvas.height - this.height);
+    this.image.src = "./enemies/enemy3.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
+    this.width = this.spriteWidth / 3; //109
+    this.height = this.spriteHeight / 3; //88
+    this.speed = Math.random() * 4 - 10;
+    this.x = Math.random() * (canvas.width - this.width); //282;
+    this.y = Math.random() * (canvas.height - this.height); //833;
     this.frame = 0;
-    this.flapSpeed = Math.floor(Math.random() * 4 + 1);
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 2 + 0.5;
+    this.curve = Math.random() * 200 + 50;
   }
   update() {
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
+    this.x =
+      (canvas.width / 2) * Math.cos((this.angle * Math.PI) / 180) +
+      (canvas.width / 2 - this.width / 2);
+    this.y =
+      this.curve * Math.cos(this.angle * (Math.PI / 180)) +
+      (canvas.height / 2 - this.height / 2);
+    this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    console.log(Math.random() * 10);
+
     //animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
